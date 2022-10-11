64f4b5773780eb2d1f3fea38996855108cda7867
wojtek.brzoska@gmail.com <grafik@up.local>
2022-10-11 12:34:30 +0200

    Dodanie penlnej funkconalnosci, dwa poziomy trudnosci, okno wyboru poziomu

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 //
 //p.addEventListener("click", e => {console.log(e.target)})
-const modal = document.querySelector(".modal-container");
-const modalTitle = document.querySelector(".modal-container h3");
-const nastepnaGra = document.querySelector(".modal-container button.again");
-const koniec = document.querySelector(".modal-container button.end");
+const modal = document.querySelector(".modal-container.game-end");
+const modalTitle = document.querySelector(".modal-container.game-end h3");
+const nastepnaGra = document.querySelector(".modal-container.game-end button.again");
+const koniec = document.querySelector(".modal-container.game-end button.end");
 
+const modalGameStart = document.querySelector(".game-start");
+const poziomTrudnosci = document.querySelectorAll(".game-start .poziom button");
+const graj = document.querySelector(".game-start .play button");
 const zwyciestwa = document.querySelector(".wynik .gracz span");
 const remisy = document.querySelector(".wynik .remis span");
 const przegrane = document.querySelector(".wynik .komputer span");
 
 let plansza = ["","","","","","","","",""]
-
+let poziom = 1;
 let wygrana = false;
 let koniecGry = false;
 
@@ -30,29 +33,82 @@ let postawKrzyzyk = (e, button, index)=>{
 };
 
 let postawKolko = () => {
-    let ileWolnychPol = plansza.filter( pole => pole==="").length;
-    //console.log("ile wolnych pol: "+ileWolnychPol);
-    let losowaLiczba = Math.floor(Math.random()  *ileWolnychPol)
-    //console.log("Losowa liczba: "+losowaLiczba)
-    let licznik = 0;
-    let kolko = true;
-    while(kolko){
-        //console.log("Licznik: "+licznik)
-        if(plansza[licznik] === ""){
-            if(losowaLiczba === 0){
-                plansza[licznik] = "O";
-                kolko=false;
-                //console.log("Postawiłek kółko: "+ licznik )
-            }else{
-                losowaLiczba--;
+    console.log("Czy są dwa X w lini: "+sprawdzCzySaDwaZnakiWLini())
+    console.log(sprawdzCzySaDwaZnakiWLini() != undefined);
+    if(sprawdzCzySaDwaZnakiWLini() != undefined && poziom === 2){
+        //console.log("Do zablokowania pole " + sprawdzCzySaDwaZnakiWLini());
+        plansza[sprawdzCzySaDwaZnakiWLini()]="O";
+        //console.log("wstaw  O w miejsce: "+plansza[sprawdzCzySaDwaZnakiWLini()]);
+        //console.log(plansza);
+    }else{
+        let ileWolnychPol = plansza.filter( pole => pole==="").length;
+        //console.log("ile wolnych pol: "+ileWolnychPol);
+        let losowaLiczba = Math.floor(Math.random()  *ileWolnychPol)
+        //console.log("Losowa liczba: "+losowaLiczba)
+        let licznik = 0;
+        let kolko = true;
+        while(kolko){
+            //console.log("Licznik: "+licznik)
+            if(plansza[licznik] === ""){
+                if(losowaLiczba === 0){
+                    plansza[licznik] = "O";
+                    kolko=false;
+                    //console.log("Postawiłek kółko: "+ licznik )
+                }else{
+                    losowaLiczba--;
+                }
+    
             }
-
+            licznik++;
+            if(licznik>10) kolko = false;
         }
-        licznik++;
-        if(licznik>10) kolko = false;
     }
     //console.log("Postaw kolko plansza: ")
     //console.log(plansza)
+}
+
+let sprawdzCzySaDwaZnakiWLini = () =>{
+    
+    if((plansza[0] + plansza[4] + plansza[8]) === "XX"){
+        console.log("Dwa x w lini do zablokowania. skos 1: "+plansza[0] +",2: "+ plansza[4] +",3: "+ plansza[8] );
+        //console.log(zrocPustePolePlanszy(0,4,8));
+        if(zrocPustePolePlanszy(0,4,8) != undefined){
+            return zrocPustePolePlanszy(0,4,8);
+        }
+    }else if((plansza[2] + plansza[4] + plansza[6]) === "XX"){
+        //console.log("Dwa x w lini do zablokowania. / " );
+        if(zrocPustePolePlanszy(2,4,6) != undefined){
+            return zrocPustePolePlanszy(2,4,6);
+        }
+    }else{
+        for(let i = 0; i < 3; i++){
+            if((plansza[(i*3)] + plansza[(i*3)+1] + plansza[(i*3)+2]) === "XX"){
+                //console.log("Dwa x w lini do zablokowania. Wiersz "+i );
+                if(zrocPustePolePlanszy((i*3),(i*3)+1,(i*3)+2) != undefined){
+                    return zrocPustePolePlanszy((i*3),(i*3)+1,(i*3)+2);
+                    break;
+                }
+            }
+            if((plansza[i] + plansza[i+3] + plansza[i+6]) === "XX"){
+                //console.log("Dwa x w lini do zablokowania. Kolumna: "+i )
+                if(zrocPustePolePlanszy(i,i+3,i+6) != undefined){
+                    return zrocPustePolePlanszy(i,i+3,i+6);
+                    break;
+                }
+            }
+        }
+    }
+}
+let zrocPustePolePlanszy = (el1, el2, el3) => {
+    if(plansza[el1] == ""){
+        return el1;
+    }else if(plansza[el2] == ""){
+        return el2;
+    }else if(plansza[el3] == ""){
+        return el3;
+    }else{
+        return null;
+    }
 }
 
 let rysujPlansze = () => {
@@ -151,7 +207,22 @@ koniec.addEventListener("click", ()=>{
     przegrane.innerHTML="0";
     zwyciestwa.innerHTML="0";
     remisy.innerHTML="0";
+    modalGameStart.classList.remove("hidden");
 
 });
+poziomTrudnosci.forEach(przycisk => przycisk.addEventListener("click", (event)=>{
+    console.log(event.target);
+    poziomTrudnosci.forEach(p => p.classList.add("disable"));
+    przycisk.classList.remove("disable");
+    if(przycisk.textContent === "Łatwy"){
+        poziom = 1;
+    }else if(przycisk.textContent === "Trudny"){
+        poziom = 2;
+    }
+    
+}));
+graj.addEventListener("click", ()=>{
+    modalGameStart.classList.add("hidden");
+})
 rysujPlansze();
 
